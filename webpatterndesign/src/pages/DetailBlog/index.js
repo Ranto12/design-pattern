d6606ea06a13f8285a5c9ae099b36c660848efc7
Ranto <rantoirwanda08@gmail.com>
2022-07-27 02:05:52 +0700

    detail project

--- webpatterndesign/src/pages/DetailBlog/index.js
+++ webpatterndesign/src/pages/DetailBlog/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import {LogoRegister} from '../../Assets'
+import { Gap } from '../../component';
+import './DetailBlog.scss';
 
 const DetailBlog = () => {
   return (
-    <div>DetailBlog</div>
+    <div className='detail-blog-wrapper'>
+      <Gap height={20}/>
+      <img className='img-cover' src={LogoRegister} alt="tumn"/>
+      <p className='blog-title'>Title Blog</p>
+      <p className='blog-author'>Author - Date Post</p>
+      <p className='blog-body'>Content Blog</p>
+    </div>
   )
 }
 
