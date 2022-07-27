178a9d6a651c754fc05cb13979ea37205dc37bdf
Ranto <rantoirwanda08@gmail.com>
2022-07-27 13:40:46 +0700

    add server

--- webpatterndesign/src/component/molecules/BlogItems/index.js
+++ webpatterndesign/src/component/molecules/BlogItems/index.js
@@ -1,6 +1,9 @@
 import {LogoRegister} from '../../../Assets/index';
+import {Button} from '../../';
 import './BlogItems.scss';
+import { useNavigate } from 'react-router';
 const BlogItems = () => {
+  const navigate = useNavigate();
   return (
     <div className='blog-items'>
         <img  className='image-tumn' src={LogoRegister} alt="post"/>
@@ -8,6 +11,7 @@ const BlogItems = () => {
             <p className='title'>Title Blog</p>
             <p className='author'>Autohor - date Post</p>
             <p className='body'> lorem impsum  </p>
+            <Button name="view detail" onClick={()=> navigate('/detail-blog')}/>
         </div>
     </div>
   )
--- webpatterndesign/src/pages/DetailBlog/index.js
+++ webpatterndesign/src/pages/DetailBlog/index.js
@@ -1,8 +1,10 @@
+import { useNavigate } from 'react-router';
 import {LogoRegister} from '../../Assets'
-import { Gap } from '../../component';
+import { Button, Gap } from '../../component';
 import './DetailBlog.scss';
 
 const DetailBlog = () => {
+  const navigate = useNavigate();
   return (
     <div className='detail-blog-wrapper'>
       <Gap height={20}/>
@@ -10,6 +12,10 @@ const DetailBlog = () => {
       <p className='blog-title'>Title Blog</p>
       <p className='blog-author'>Author - Date Post</p>
       <p className='blog-body'>Content Blog</p>
+      <div className="button-back">
+      <Button name="back" onClick={()=>navigate('/')}/>
+      </div>
+      <Gap height={20}/>
     </div>
   )
 }
